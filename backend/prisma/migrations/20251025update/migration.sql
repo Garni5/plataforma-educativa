2ca1ddc63f61e0cba5ea5fd281ca67483d94904e
Saul Alejandro Peñaloza Lujan <142471256+saulRAgitub@users.noreply.github.com>
2025-10-28 03:59:14 -0400

    cambios en el back quitando telefono,ci y ajustando test y frontend

--- backend/prisma/migrations/20251025update/migration.sql
+++ backend/prisma/migrations/20251025update/migration.sql
@@ -2,5 +2,3 @@
 ALTER TABLE "persona" DROP COLUMN "ci";
 ALTER TABLE "persona" DROP COLUMN "telefono";
 
--- Add new column
-ALTER TABLE "persona" ADD COLUMN "confirmar_password" VARCHAR(100) NOT NULL;
